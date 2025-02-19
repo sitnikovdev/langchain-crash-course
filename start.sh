0f4e30b817fa6ad048a5ba33c689b49ba7b2c443
dev2024 <oleg.sitnikov@icloud.com>
2025-02-20 02:03:30 +0300

    Save Jupiter Session

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -p 8888:8888 -v $(pwd):/app langchain-docker-app
+docker run -it --rm -p 8888:8888 -v $(pwd):/app -v $(pwd)/.jupyter:/root/.jupyter -v jupyter-data:/root/.local langchain-docker-app
